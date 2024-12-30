8cc8a418387f45d4cc4795be6d167644e95e1824
yoshi8-h <hirao.y.freelance@gmail.com>
2024-12-30 14:10:46 +0900

    news-page セクションのページネーション完成時

--- WordPressTheme/home.php
+++ WordPressTheme/home.php
@@ -22,41 +22,27 @@
     <section class="news-page l-news-page">
       <div class="news-page__inner inner">
         <div class="news-page__posts posts">
-          <div class="posts__item post post--main js-fadeInUp">
-            <div class="post__meta">
-              <div class="post__category is-news">お知らせ</div>
-              <time class="post__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
-            </div>
-            <a href="" class="post__link-text">投稿のテキストが入ります。投稿のテキストが入ります。投稿のテキストが入ります。投稿のテキストが入ります。投稿のテキストが入ります。投稿のテキストが入ります。</a>
-          </div>
-          <div class="posts__item post post--main js-fadeInUp">
-            <div class="post__meta">
-              <div class="post__category is-topics">トピックス</div>
-              <time class="post__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
-            </div>
-            <a href="" class="post__link-text">投稿のテキストが入ります。</a>
-          </div>
-          <div class="posts__item post post--main js-fadeInUp">
-            <div class="post__meta">
-              <div class="post__category is-other">その他</div>
-              <time class="post__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
-            </div>
-            <a href="" class="post__link-text">投稿のテキストが入ります。投稿のテキストが入ります。</a>
-          </div>
-          <div class="posts__item post post--main js-fadeInUp">
-            <div class="post__meta">
-              <div class="post__category is-topics">トピックス</div>
-              <time class="post__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
-            </div>
-            <a href="" class="post__link-text">投稿のテキストが入ります。</a>
-          </div>
-          <div class="posts__item post post--main js-fadeInUp">
-            <div class="post__meta">
-              <div class="post__category is-other">その他</div>
-              <time class="post__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
-            </div>
-            <a href="" class="post__link-text">投稿のテキストが入ります。投稿のテキストが入ります。</a>
-          </div>
+          <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : ?>
+              <?php the_post(); ?>
+              <div class="posts__item post post--main js-fadeInUp">
+                <div class="post__meta">
+                  <div class="post__category is-news">お知らせ</div>
+                  <time class="post__date" datetime="<?php the_time('c'); ?>"><?php the_time('Y.m.d'); ?></time>
+                </div>
+                <a href="<?php the_permalink(); ?>" class="post__link-text">
+                  <?php
+                    $content = strip_tags(get_the_content()); // HTMLタグを除去
+                    echo mb_substr($content, 0, 120) . '...'; // 120文字まで切り取り、省略記号を付加
+                  ?>
+                </a>
+              </div>
+            <?php endwhile; ?>
+          <?php endif; ?>
+        </div>
+        <!-- ページネーション -->
+        <div class="news-page__pagenavi">
+          <?php get_template_part('parts/wp-pagenavi'); ?>  <!-- 拡張子「.php」は不要。相対パスではないので先頭に「./」不要。 -->
         </div>
       </div>
     </section>
