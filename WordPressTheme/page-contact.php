ff98ce943bf1a2fa5dd546a616562cc227c7b256
yoshi8-h <hirao.y.freelance@gmail.com>
2025-01-10 16:38:29 +0900

    お問い合わせページの動的化直前 (見た目部分は一旦完成時)

--- WordPressTheme/page-contact.php
+++ WordPressTheme/page-contact.php
@@ -182,129 +182,133 @@
           </div>
 
           <!-- 確認画面 -->
-          <div class="contact-page__form-section-wrap">
-            <div class="contact-page__form-title title05">お問い合わせ情報の記入内容をご確認ください。</div>
-            <div class="contact-page__form-wrap">
-              <form class="contact-page__form form" novalidate>  <!-- 確認画面用 -->
-                <div class="form__fields fields">
-                  <div class="fields__item field">
-                    <div class="field__head">
-                      <label for="your-campaign" class="field__label">
-                        <span class="field__label-require">必須</span>
-                        <span class="field__label-text">お問い合わせ種別</span>
-                      </label>
+          <div class="contact-page__confirm-area">
+            <div class="contact-page__form-section-wrap">
+              <div class="contact-page__form-title title05">お問い合わせ情報の記入内容をご確認ください。</div>
+              <div class="contact-page__form-wrap">
+                <form class="contact-page__form form" novalidate>  <!-- 確認画面用 -->
+                  <div class="form__fields fields">
+                    <div class="fields__item field">
+                      <div class="field__head">
+                        <label for="your-campaign" class="field__label">
+                          <span class="field__label-require">必須</span>
+                          <span class="field__label-text">お問い合わせ種別</span>
+                        </label>
+                      </div>
+                      <div class="field__select-wrap">
+                        <span class="confirm-text"></span>
+                      </div>
                     </div>
-                    <div class="field__select-wrap">
-                      <span class="confirm-text"></span>
+                    <div class="fields__item field">
+                      <div class="field__head">
+                        <label for="your-name-kanji" class="field__label">
+                          <span class="field__label-require">必須</span>
+                          <span class="field__label-text">お名前(漢字)</span>
+                        </label>
+                      </div>
+                      <div class="field__item input">
+                        <span class="confirm-text"></span>
+                      </div>
+                    </div>
+                    <div class="fields__item field">
+                      <div class="field__head">
+                        <label for="your-name-kana" class="field__label">
+                          <span class="field__label-require">必須</span>
+                          <span class="field__label-text">お名前(カナ)</span>
+                        </label>
+                      </div>
+                      <div class="field__item input">
+                        <span class="confirm-text"></span>
+                      </div>
+                    </div>
+                    <div class="fields__item field">
+                      <div class="field__head">
+                        <label for="your-company" class="field__label">
+                          <span class="field__label-any">任意</span>
+                          <span class="field__label-text">社名</span>
+                        </label>
+                      </div>
+                      <div class="field__item input">
+                        <span class="confirm-text"></span>
+                      </div>
+                    </div>
+                    <div class="fields__item field">
+                      <div class="field__head">
+                        <label for="your-email" class="field__label">
+                          <span class="field__label-require">必須</span>
+                          <span class="field__label-text">メールアドレス</span>
+                        </label>
+                      </div>
+                      <div class="field__item input">
+                        <span class="confirm-text"></span>
+                      </div>
+                    </div>
+                    <div class="fields__item field">
+                      <div class="field__head">
+                        <label for="your-email-confirm" class="field__label">
+                          <span class="field__label-require">必須</span>
+                          <span class="field__label-text">確認用メールアドレス</span>
+                        </label>
+                      </div>
+                      <div class="field__item input">
+                        <span class="confirm-text"></span>
+                      </div>
+                    </div>
+                    <div class="fields__item field">
+                      <div class="field__head">
+                        <label for="your-tel" class="field__label">
+                          <span class="field__label-any">任意</span>
+                          <span class="field__label-text">電話番号</span>
+                        </label>
+                      </div>
+                      <div class="field__item input">
+                        <span class="confirm-text"></span>
+                      </div>
+                    </div>
+                    <div class="fields__item field is-textarea">  <!-- 『テキストエリア』の項目 -->
+                      <div class="field__head is-textarea">
+                        <label for="your-message" class="field__label">
+                          <span class="field__label-require">必須</span>
+                          <span class="field__label-text">お問合せ内容<span class="field__label-text-limit">※2,000字以内</span></span>
+                        </label>
+                      </div>
+                      <div class="field__item is-textarea">
+                        <span class="confirm-text"></span>
+                      </div>
                     </div>
                   </div>
-                  <div class="fields__item field">
-                    <div class="field__head">
-                      <label for="your-name-kanji" class="field__label">
-                        <span class="field__label-require">必須</span>
-                        <span class="field__label-text">お名前(漢字)</span>
-                      </label>
-                    </div>
-                    <div class="field__item input">
-                      <span class="confirm-text"></span>
-                    </div>
+                  <p class="form__confirm-text">
+                    「送信する」ボタンを押下すると、ご入力いただいたメールアドレスに自動返信メールが送信されます。<br
+                    >自動返信メールが届かない場合は、ご入力のメールアドレスに誤りがある可能性がございます。その場合は、再度お問い合わせいただきますようお願いいたします。<br
+                    ><span class="form__confirm-text-alert">※入力ページに戻る場合、ブラウザの戻る機能で戻ると、入力した内容がリセットされますのでご注意ください。</span>
+                  </p>
+                  <div class="form__confirm-buttons">
+                    <button type="button" value="戻る" class="form__returnbtn btn2 btn2--return">
+                      <div class="btn2__arrow"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/btn2-arrow-white-return.webp" alt="←"></div>
+                      <div class="btn2__text">戻る</div>
+                    </button>
+                    <button type="submit" value="送信する" class="form__sendbtn btn2">
+                      <div class="btn2__text">送信する</div>
+                      <div class="btn2__arrow"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/btn2-arrow-white.webp" alt="→"></div>
+                    </button>
                   </div>
-                  <div class="fields__item field">
-                    <div class="field__head">
-                      <label for="your-name-kana" class="field__label">
-                        <span class="field__label-require">必須</span>
-                        <span class="field__label-text">お名前(カナ)</span>
-                      </label>
-                    </div>
-                    <div class="field__item input">
-                      <span class="confirm-text"></span>
-                    </div>
-                  </div>
-                  <div class="fields__item field">
-                    <div class="field__head">
-                      <label for="your-company" class="field__label">
-                        <span class="field__label-any">任意</span>
-                        <span class="field__label-text">社名</span>
-                      </label>
-                    </div>
-                    <div class="field__item input">
-                      <span class="confirm-text"></span>
-                    </div>
-                  </div>
-                  <div class="fields__item field">
-                    <div class="field__head">
-                      <label for="your-email" class="field__label">
-                        <span class="field__label-require">必須</span>
-                        <span class="field__label-text">メールアドレス</span>
-                      </label>
-                    </div>
-                    <div class="field__item input">
-                      <span class="confirm-text"></span>
-                    </div>
-                  </div>
-                  <div class="fields__item field">
-                    <div class="field__head">
-                      <label for="your-email-confirm" class="field__label">
-                        <span class="field__label-require">必須</span>
-                        <span class="field__label-text">確認用メールアドレス</span>
-                      </label>
-                    </div>
-                    <div class="field__item input">
-                      <span class="confirm-text"></span>
-                    </div>
-                  </div>
-                  <div class="fields__item field">
-                    <div class="field__head">
-                      <label for="your-tel" class="field__label">
-                        <span class="field__label-any">任意</span>
-                        <span class="field__label-text">電話番号</span>
-                      </label>
-                    </div>
-                    <div class="field__item input">
-                      <span class="confirm-text"></span>
-                    </div>
-                  </div>
-                  <div class="fields__item field is-textarea">  <!-- 『テキストエリア』の項目 -->
-                    <div class="field__head is-textarea">
-                      <label for="your-message" class="field__label">
-                        <span class="field__label-require">必須</span>
-                        <span class="field__label-text">お問合せ内容<span class="field__label-text-limit">※2,000字以内</span></span>
-                      </label>
-                    </div>
-                    <div class="field__item is-textarea">
-                      <span class="confirm-text"></span>
-                    </div>
-                  </div>
-                </div>
-                <p class="form__confirm-text">
-                  「送信する」ボタンを押下すると、ご入力いただいたメールアドレスに自動返信メールが送信されます。<br
-                  >自動返信メールが届かない場合は、ご入力のメールアドレスに誤りがある可能性がございます。その場合は、再度お問い合わせいただきますようお願いいたします。<br
-                  ><span class="form__confirm-text-alert">※入力ページに戻る場合、ブラウザの戻る機能で戻ると、入力した内容がリセットされますのでご注意ください。</span>
-                </p>
-                <div class="form__confirm-buttons">
-                  <button type="button" value="戻る" class="form__returnbtn btn2 btn2--return">
-                    <div class="btn2__arrow"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/btn2-arrow-white-return.webp" alt="←"></div>
-                    <div class="btn2__text">戻る</div>
-                  </button>
-                  <button type="submit" value="送信する" class="form__sendbtn btn2">
-                    <div class="btn2__text">送信する</div>
-                    <div class="btn2__arrow"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/btn2-arrow-white.webp" alt="→"></div>
-                  </button>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
 
           <!-- 送信完了画面 -->
-          <div class="contact-page__form-section-wrap">
-            <div class="contact-page__form-title title05">お問い合わせ完了</div>
-            <div class="contact-page__form-wrap">
-              <p class="contact-page__complete-text">
-                お問い合わせありがとうございました。<br
-                >お問い合わせ内容確認後、担当者からご連絡をさせて頂きます。
-              </p>
-              <div class="contact-page__complete-btn-wrap">
-                <a href="<?php echo esc_url(home_url('/')); ?>" class="contact-page__complete-btn">トップ画面に戻る　＞</a>
+          <div class="contact-page__thanks-area">
+            <div class="contact-page__form-section-wrap">
+              <div class="contact-page__form-title title05">お問い合わせ完了</div>
+              <div class="contact-page__form-wrap">
+                <p class="contact-page__complete-text">
+                  お問い合わせありがとうございました。<br
+                  >お問い合わせ内容確認後、担当者からご連絡をさせて頂きます。
+                </p>
+                <div class="contact-page__complete-btn-wrap">
+                  <a href="<?php echo esc_url(home_url('/')); ?>" class="contact-page__complete-btn">トップ画面に戻る　＞</a>
+                </div>
               </div>
             </div>
           </div>
